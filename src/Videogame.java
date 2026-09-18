8a63719d4cd83e6d694a4c4b15738944f132df99
Kauli  Kleemann <0431746@feevale.br>
2026-09-18 11:15:27 -0300

    Cria os métodos getters e setters da classe Videogame

--- src/Videogame.java
+++ src/Videogame.java
@@ -9,4 +9,29 @@ public class Videogame extends Hardware {
         this.sensorMovimento = sensorMovimento;
         this.leitorDisco = leitorDisco;
     }
+
+    public String getVersao() {
+        return versao;
+    }
+
+    public void setVersao(String versao) {
+        this.versao = versao;
+    }
+
+    public boolean isSensorMovimento() {
+        return sensorMovimento;
+    }
+
+    public void setSensorMovimento(boolean sensorMovimento) {
+        this.sensorMovimento = sensorMovimento;
+    }
+
+    public boolean isLeitorDisco() {
+        return leitorDisco;
+    }
+
+    public void setLeitorDisco(boolean leitorDisco) {
+        this.leitorDisco = leitorDisco;
+    }
+
 }
