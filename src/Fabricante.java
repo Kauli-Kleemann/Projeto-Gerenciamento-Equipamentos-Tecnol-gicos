d7ae1954719432321a00eaf9c77a29bb08d94933
Kauli  Kleemann <0431746@feevale.br>
2026-09-18 09:27:17 -0300

    Cria os atributos da classe Fabricante e seu construtor

---
+++ src/Fabricante.java
@@ -0,0 +1,12 @@
+public class Fabricante {
+    private String nome;
+    private String site;
+    private String paisOrigem;
+
+    public Fabricante(String nome, String site, String paisOrigem) {
+        this.nome = nome;
+        this.site = site;
+        this.paisOrigem = paisOrigem;
+    }
+
+}
